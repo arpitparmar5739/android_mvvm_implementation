068146c87ac3802f79ff10220a163e8016688524
Arpit Parmar <arpitparmar5739@gmail.com>
2019-11-25 16:41:10 +0530

    1. Implemented NoteViewModel. 2. Fixed a bug in NoteDatabase.

--- app/src/main/java/com/example/mvvmimplementation/MainActivity.java
+++ app/src/main/java/com/example/mvvmimplementation/MainActivity.java
@@ -1,14 +1,28 @@
 package com.example.mvvmimplementation;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProviders;
 
 import android.os.Bundle;
+import android.widget.Toast;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+    private NoteViewModel noteViewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        noteViewModel = ViewModelProviders.of(this).get(NoteViewModel.class);
+        noteViewModel.getAllNotes().observe(this, new Observer<List<Note>>() {
+            @Override
+            public void onChanged(List<Note> notes) {
+                Toast.makeText(MainActivity.this, "onChanged", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
--- app/src/main/java/com/example/mvvmimplementation/NoteDatabase.java
+++ app/src/main/java/com/example/mvvmimplementation/NoteDatabase.java
@@ -17,7 +17,7 @@ public abstract class NoteDatabase extends RoomDatabase {
     public abstract NoteDao noteDao();
 
     public static synchronized NoteDatabase getInstance(Context context) {
-        if (instance != null) {
+        if (instance == null) {
             instance = Room.databaseBuilder(
                     context.getApplicationContext(),
                     NoteDatabase.class, "note_database"
